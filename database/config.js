83f88b54e31665b9ec8610c86da802eba193b8c4
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-28 15:57:28 -0300

    Update database connection and add console log message

--- database/config.js
+++ database/config.js
@@ -4,12 +4,17 @@ const dbConnection = async() => {
 
     try {
         
-        mongoose.connect(process.env.DB_CONNECTION);
+        await mongoose.connect(process.env.DB_CONNECTION);
 
+        console.log('Base de datos online');
 
     } catch (error) {
         console.log(error);
         throw new Error('Error al iniciar la base de datos');
     }
 
+}
+
+module.exports = {
+    dbConnection
 }
