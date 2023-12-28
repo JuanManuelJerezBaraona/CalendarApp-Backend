f5ad2e1dcb907dd2669261175ff9fde6d5d9e1b1
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-28 15:45:15 -0300

    Add .env and untitled/ to .gitignore, and update dependencies in package.json

---
+++ database/config.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+
+    try {
+        
+        mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error al iniciar la base de datos');
+    }
+
+}
