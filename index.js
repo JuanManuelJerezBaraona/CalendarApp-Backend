5526e232c8018cfee2bdce7fab4f24e3b92df988
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-29 17:58:11 -0300

    Add CORS middleware to enable cross-origin resource sharing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { dbConnection } = require('./database/config');
 require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
 
 
 // Crear el servidor de express
@@ -8,6 +9,9 @@ const app = express();
 
 // Base de Datos
 dbConnection();
+
+// CORS
+app.use(cors());
 
 // Directorio Público
 app.use(express.static('public'));
